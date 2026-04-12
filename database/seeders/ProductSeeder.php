c88d854c711f94640bc35ecf8e5eabeab47685ab
ggz2468 <guiguzi19971006@gmail.com>
2026-04-12 18:06:06 +0800

    更新儲存於資料表的產品圖片路徑

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -27,7 +27,7 @@ class ProductSeeder extends Seeder
 
         $products->each(function (Product $product) {
             Image::factory()->create([
-                'url' => '/public/images/products/' . $product->name . '.gif',
+                'url' => '/images/products/' . $product->name . '.gif',
                 'imageable_id' => $product->id,
                 'imageable_type' => Product::class
             ]);
