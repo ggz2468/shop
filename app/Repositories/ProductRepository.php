ac2c318288cb48c5929defbcf5546758c813b002
ggz2468 <guiguzi19971006@gmail.com>
2026-04-14 01:30:14 +0800

    新增 Cache 儲存機制

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -2,7 +2,7 @@
 
 namespace App\Repositories;
 
-use App\Models\Product;
+use Illuminate\Support\Facades\Cache;
 
 class ProductRepository extends Repository
 {
@@ -13,18 +13,40 @@ class ProductRepository extends Repository
      */
     public function getAllProducts()
     {
-        $products = $this->get([], ['created_at', 'desc']);
-        $products = $products->map(function (Product $product) {
-            $product->load('images');
-            return [
-                'id' => $product->id,
-                'product_spec_id' => $product->product_spec_id,
-                'name' => $product->name,
-                'price' => $product->price,
-                'description' => $product->description,
-                'image_path' => $product->images->first()?->url,
-            ];
-        })->all();
-        return $products;
+        // 取得所有產品編號
+        $productIds = Cache::remember('all_product_ids', 3600, function () {
+            return $this->modelClassName::pluck('id')->all();
+        });
+
+        // 將產品編號組合成 Cache key
+        $cacheKeys = array_map(fn ($id) => "product:$id", $productIds);
+
+        // 取得存在 Cache 中的產品資料
+        $products = Cache::many($cacheKeys);
+
+        // 取得不存在於 Cache 中的產品編號
+        $missingProductIds = array_map(fn ($key) => (int) str_replace('product:', '', $key), array_filter($cacheKeys, fn ($key) => !isset($products[$key])));
+
+        // 如果有缺少的產品資料，從資料庫中取得並存入 Cache
+        foreach ($missingProductIds as $productId) {
+            $product = $this->modelClassName::find($productId);
+
+            if (!empty($product)) {
+                $product->load('images');
+                $product = [
+                    'id' => $product->id,
+                    'product_spec_id' => $product->product_spec_id,
+                    'name' => $product->name,
+                    'price' => $product->price,
+                    'description' => $product->description,
+                    'image_path' => $product->images->first()?->url,
+                ];
+                $cacheKey = "product:$productId";
+                Cache::put($cacheKey, $product, 3600);
+                $products[$cacheKey] = $product;
+            }
+        }
+
+        return array_values($products);
     }
 }
