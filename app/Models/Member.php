a4bb40d2edab5bd51c3a48bd6dfbb3360c193bb6
ggz2468 <guiguzi19971006@gmail.com>
2026-05-11 01:57:46 +0800

    [更新] - 更新欄位定義

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -2,23 +2,25 @@
 
 namespace App\Models;
 
+use App\Enums\Member\Gender;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 
 class Member extends Authenticatable
 {
     use HasApiTokens;
     use HasFactory;
     use SoftDeletes;
+    use Notifiable;
 
     protected $fillable = [
         'first_name',
         'last_name',
-        'password',
         'birth_date',
         'address',
         'gender',
@@ -26,18 +28,20 @@ class Member extends Authenticatable
 
     protected $hidden = [
         'national_id_number',
+        'email',
+        'email_verified_at',
         'phone',
-        'birth_date',
-        'address',
-        'gender',
+        'phone_verified_at',
         'password',
         'deleted_at',
     ];
 
     protected $casts = [
-        'birth_date' => 'date',
-        'gender' => 'integer',
+        'email_verified_at' => 'datetime',
+        'phone_verified_at' => 'datetime',
         'password' => 'hashed',
+        'birth_date' => 'date',
+        'gender' => Gender::class,
     ];
 
     public function orders(): HasMany
@@ -48,5 +52,10 @@ class Member extends Authenticatable
     public function images(): MorphMany
     {
         return $this->morphMany(Image::class, 'imageable');
+    }
+
+    public function routeNotificationForVonage(object $notification): ?string
+    {
+        return $this->phone;
     }
 }
