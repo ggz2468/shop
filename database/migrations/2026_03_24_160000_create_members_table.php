0a2183bffcd1202bf525866b4e35f99225f4669f
ggz2468 <guiguzi19971006@gmail.com>
2026-05-12 22:09:40 +0800

    [更新] - 更新資料表欄位定義

--- database/migrations/2026_03_24_160000_create_members_table.php
+++ database/migrations/2026_03_24_160000_create_members_table.php
@@ -13,17 +13,17 @@ return new class extends Migration
     {
         Schema::create('members', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name', 50)->comment('名字');
-            $table->string('last_name', 50)->comment('姓氏');
+            $table->string('first_name', 50)->nullable()->default(null)->comment('名字');
+            $table->string('last_name', 50)->nullable()->default(null)->comment('姓氏');
             $table->char('national_id_number', 10)->comment('身分證字號');
             $table->string('email', 100)->comment('電子郵件');
             $table->dateTime('email_verified_at')->nullable()->default(null)->comment('電子郵件驗證時間');
             $table->string('phone', 20)->comment('手機號碼');
             $table->dateTime('phone_verified_at')->nullable()->default(null)->comment('手機號碼驗證時間');
             $table->string('password', 255)->comment('密碼');
-            $table->date('birth_date')->comment('生日');
-            $table->text('address')->comment('住址');
-            $table->unsignedTinyInteger('gender')->comment('性別');
+            $table->date('birth_date')->nullable()->default(null)->comment('生日');
+            $table->text('address')->nullable()->default(null)->comment('住址');
+            $table->unsignedTinyInteger('gender')->nullable()->default(null)->comment('性別');
             $table->rememberToken();
             $table->timestamp('created_at')->nullable()->useCurrent();
             $table->timestamp('updated_at')->nullable()->useCurrent()->useCurrentOnUpdate();
